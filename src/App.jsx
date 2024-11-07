b3120a92344feb80f072ffbd6c93736b433b0aee
Le-Jr <nascimentoleandrojr@gmail.com>
2024-11-07 16:29:35 -0300

    Setup inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import "./index.scss";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-center ubuntu my-4">
+        Rick and Morty <span className="text-primary">API</span>
+      </h1>
+    </>
+  );
+};
+
+export default App;
